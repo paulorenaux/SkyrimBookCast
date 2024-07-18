6cc7e5aaf4c5d76f0ae87b4e5625b8fc998d654b
Paulo <paulo_renaux@hotmail.com>
2024-07-18 15:59:55 -0300

    fixes and improvements: - book dictionary as typed dictionary for more cohesion while accessing key+values - cleanup on SkyrimBookCast - added typing to functions used - tested manually

--- SkyrimBookCast.py
+++ SkyrimBookCast.py
@@ -1,11 +1,24 @@
-from bookScraper import scrape
+from bookScraper import scrape, Book
 from gtts import gTTS
-import time
 import os
 
+def find_file(name: str, path: str) -> list:
+  result = []
+  for root, dir, files in os.walk(path):
+    if name in files:
+      result.append(os.path.join(root, name))
+  return result
+
+def book_to_audio(book: Book) -> None:
+  contents = f"{book['title']} {book['contents']}"
+  filename = f"{book['filename']}.mp3"
+  files = find_file(filename, './')
+  if len(files) == 0:
+    audio = gTTS(contents, lang='en', slow=False)
+    audio.save(filename)
+  return filename
+
 books = scrape()
-for book in books[0:1]:
-  audio = gTTS(text = f"{book['title']} {book['contents']}", lang='en', slow=False)
-  filename = f"{book['title']}.mp3"
-  audio.save(filename)
-  os.system(f'start {filename}')
+for book in books[1:2]:
+  audio_filename = book_to_audio(book)
+  os.system(f"start {audio_filename}")
--- bookScraper.py
+++ bookScraper.py
@@ -2,12 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from jsonFile import serialize, parse
 import re
+from typing import TypedDict
 
-# File where link to each book is cached
-_book_links_file = 'book_links.json'
+class Book(TypedDict):
+  book_links_file = 'book_links.json'
+  """File where link to each book is cached"""
+  title: str
+  file_name: str
+  link: str
+  contents: str
 
-def _get_cached_books() -> dict: return parse(_book_links_file)
-def _save_cached_books(books: dict): serialize(books, _book_links_file)
+def _get_cached_books() -> dict: 
+  try:
+    return parse(Book.book_links_file)
+  except:
+    return None
+def _save_cached_books(books: dict) -> None: serialize(books, Book.book_links_file)
 def _clean_file_name (file_name: str) -> str: return re.sub('[ ,:.]', '_', file_name)
 
 def _get_books_uesp () -> list:
@@ -27,12 +37,11 @@ def _get_books_uesp () -> list:
       continue
 
     anchor = td[1].find_element(By.TAG_NAME, 'a')
-    book = {
+    book: Book = {
       'title': anchor.get_attribute('title'),
       'filename': _clean_file_name(anchor.get_attribute('title')),
       'link': anchor.get_attribute('href')
     }
-    
     books.append(book)
     
   return books
@@ -42,11 +51,11 @@ def _get_books () -> dict:
   """if book title and url list is cached, retrieve from cache; retrieve from uesp otherwise."""
   books = _get_cached_books()
   if (books == None):
-    books = _get_books_uesp(True)
+    books = _get_books_uesp()
     _save_cached_books(books)
   return books
 
-def _get_book_contents_uesp(url):
+def _get_book_contents_uesp(url) -> str:
   """Get book contents for each link."""
   """TODO: first letter in first paragraph is not a text, but an image"""
   driver = webdriver.Chrome()
@@ -55,7 +64,7 @@ def _get_book_contents_uesp(url):
   return book_contents.text
 # end get_book_contents
 
-def scrape():
+def scrape() -> dict:
   books =_get_books()
   for book in books[0:5]:
     if ('contents' not in book):
